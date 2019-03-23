9d80bbc0ddfec2a0f8cc9fecd6c557008e119d2b
Ben Teichman <ben.teichman@groupbyinc.com>
2019-03-23 01:44:31 -0400

    add automated react plugin tests

--- plugins/react/src/index.test.ts
+++ plugins/react/src/index.test.ts
@@ -1,5 +1,6 @@
 // tslint:disable:no-expression-statement
 import test from 'ava';
+import * as React from 'react';
 import { main } from '.';
 
 test('includes keys', t => {
@@ -8,4 +9,20 @@ test('includes keys', t => {
   t.plan(keys.length);
 
   ['createElement', 'render'].forEach(key => t.true(keys.includes(key)));
+});
+
+test('createElement()', t => {
+  t.is(main.createElement, React.createElement);
+});
+
+test('render()', t => {
+  const elementId = 'myApp';
+
+  const rootEl = document.createElement('div');
+  rootEl.setAttribute('id', elementId);
+  document.body.appendChild(rootEl);
+
+  main.render(React.createElement('h1', null, 'Hello, World!'), elementId);
+
+  t.is(document.getElementById(elementId).innerHTML, '<h1>Hello, World!</h1>');
 });
