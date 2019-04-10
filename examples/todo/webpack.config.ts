cfc8dba01639f8813785424ee6b0bdb872473760
Ben Teichman <ben.l.teichman@gmail.com>
2019-04-09 23:15:14 -0400

    feat: add simple todo implementation (#59)

---
+++ examples/todo/webpack.config.ts
@@ -0,0 +1,30 @@
+import KnotPlugin from '@knot/webpack-plugin';
+import * as HtmlPlugin from 'html-webpack-plugin';
+
+export default {
+  entry: './src/index.js',
+  output: {
+    filename: 'app.js',
+    path: __dirname + '/dist',
+  },
+
+  resolve: {
+    extensions: ['.kn', '.js', '.json'],
+  },
+
+  plugins: [
+    new HtmlPlugin({
+      template: 'index.html',
+    }),
+    new KnotPlugin({
+      // debug: true,
+      knot: process.env.KNOT_BINARY,
+    }),
+  ],
+
+  devServer: {
+    historyApiFallback: true,
+    open: true,
+    port: 1337,
+  },
+};
