6135fb22c2601387c0a7a4abd36f5d19a03fcad2
Ben Teichman <ben.teichman@groupbyinc.com>
2019-04-14 15:34:09 -0400

    fix(release): use monorepo with hooks

---
+++ .releaserc.js
@@ -0,0 +1,28 @@
+const hooks = require('semantic-release-monorepo-hooks');
+const outpute = hooks();
+
+module.exports = {
+  prepare: [
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    {
+      'path': '@semantic-release/git',
+      'message': `chore(${output.package}): release \${nextRelease.version} [skip ci]\n\n\${nextRelease.notes}`
+    }
+  ],
+  publish: [
+    '@semantic-release/npm'
+  ],
+  verifyConditions: [
+    '@semantic-release/npm',
+    '@semantic-release/git'
+  ],
+  monorepo: {
+    analyzeCommits: [
+      '@semantic-release/commit-analyzer'
+    ],
+    generateNotes: [
+      '@semantic-release/release-notes-generator'
+    ]
+  }
+};
