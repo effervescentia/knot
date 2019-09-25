3bdc470b8e7fb9e7fd21c0c6a26c98d8ec603516
Ben Teichman <ben.l.teichman@gmail.com>
2019-09-25 18:08:55 -0400

    fix: only install if project has been built

--- packages/binaries/compiler/install.js
+++ packages/binaries/compiler/install.js
@@ -1,3 +1,11 @@
 #!/usr/bin/env node
 
-require('./build/main/install').default();
+const install = (function() {
+  try {
+    return require('./build/main/install').default;
+  } catch (e) {
+    console.log('install is not possible until project has been built');
+  }
+})();
+
+install && install(__dirname);
