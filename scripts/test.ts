cff291b6c9612aa9c7891fc119fc25eb781e6fa4
Ben Teichman <ben.l.teichman@gmail.com>
2020-07-10 01:51:31 -0400

    ci(circleci): store ava and eslint reports

--- scripts/test.ts
+++ scripts/test.ts
@@ -1,4 +1,6 @@
-import { series } from 'nps-utils';
+import { concurrent, series } from 'nps-utils';
+
+const eslintCommand = 'eslint --quiet "src/**/*.ts"';
 
 export default {
   default: {
@@ -9,14 +11,34 @@ export default {
   ci: {
     description:
       'build project then run code linters and unit tests with coverage',
-    script: series.nps('build', 'test.lint', 'test.unit', 'cov.lcov')
+    script: series(
+      'mkdir -p reports',
+      series.nps(
+        'build',
+        'test.lint.ci',
+        'test.unit --tap | tap-xunit > reports/ava.xml',
+        'cov.lcov'
+      )
+    )
   },
   lint: {
-    description: 'run code linters',
-    script: series(
-      'eslint --quiet "src/**/*.ts"',
-      'prettier "src/**/*.ts" --list-different'
-    )
+    default: {
+      description: 'run code linters',
+      script: concurrent.nps('test.lint.eslint', 'test.lint.prettier')
+    },
+
+    ci: concurrent.nps(
+      'test.lint.eslint --format junit > reports/eslint.xml',
+      'test.lint.prettier'
+    ),
+    eslint: {
+      description: 'run eslint',
+      script: eslintCommand
+    },
+    prettier: {
+      description: 'run prettier',
+      script: 'prettier "src/**/*.ts" --list-different'
+    }
   },
   unit: {
     description: 'run unit tests',
