9f5fe89416a29870ffe203eaa4c60861ef2b4919
Ben Teichman <ben.l.teichman@gmail.com>
2022-07-06 00:46:41 -0400

    test: fix jss plugin tests

--- packages/plugins/compiler/jss/src/main.test.ts
+++ packages/plugins/compiler/jss/src/main.test.ts
@@ -1,28 +1,13 @@
 import test from 'ava';
 
-import colors from './colors';
 import main from './main';
-import properties from './properties';
-import units from './units';
 
 test('includes keys', (t) => {
   const keys = Object.keys(main);
 
   t.plan(keys.length);
 
-  [
-    ...Object.keys(properties),
-    ...Object.keys(colors),
-    ...Object.keys(units),
-
-    'visibility',
-    'hidden',
-    'none',
-
-    'num',
-    'str',
-
-    'resolve',
-    'classes',
-  ].forEach((key) => t.true(keys.includes(key)));
+  ['resolve', 'classes', 'styleExpressionPlugin', 'styleRulePlugin'].forEach(
+    (key) => t.true(keys.includes(key))
+  );
 });
