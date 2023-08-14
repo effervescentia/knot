85892a250adb0e11c50eecd5a763fff020f70e38
Ben Teichman <ben@voiceflow.com>
2023-08-14 05:19:14 -0400

    feat: working stream utils

--- combo/src/scratch.rs
+++ combo/src/scratch.rs
@@ -1,28 +1,119 @@
-use std::{cell::RefCell, rc::Rc};
+use std::{
+    cell::{OnceCell, Ref, RefCell},
+    rc::{Rc, Weak},
+};
 
-enum Stream<'a, T> {
-    Next(Rc<T>, Box<dyn FnOnce() -> Stream<'a, T> + 'a>),
-    Nil,
+pub struct State<T> {
+    position: RefCell<usize>,
+    buffer: Vec<T>,
+    iterator: OnceCell<Box<dyn Iterator<Item = T>>>,
 }
 
-fn iter_to_stream<'a, T, I>(iter: &'a RefCell<I>) -> Stream<'a, T>
-where
-    I: Iterator<Item = T>,
-{
-    match iter.borrow_mut().next() {
-        Some(value) => {
-            let value_rc = Rc::new(value);
-            let next = || iter_to_stream(iter);
+impl<T> State<T> {
+    pub fn new(iterator: Box<dyn Iterator<Item = T>>) -> Self {
+        let state = State {
+            position: RefCell::new(0 as usize),
+            buffer: vec![],
+            iterator: OnceCell::new(),
+        };
 
-            Stream::Next(value_rc, Box::new(next))
+        state.iterator.set(iterator).ok();
+
+        state
+    }
+
+    pub fn get_position(&self) -> Ref<'_, usize> {
+        self.position.borrow()
+    }
+
+    fn with_iterator(&mut self, f: impl FnOnce(&mut Self, &mut Box<dyn Iterator<Item = T>>)) {
+        self.iterator.take().map(|mut iterator| {
+            f(self, &mut iterator);
+            self.iterator.set(iterator).ok();
+        });
+    }
+
+    pub fn advance_to(&mut self, target: usize) {
+        let mut position = *self.get_position();
+
+        while position < target {
+            self.with_iterator(|state, iterator| {
+                iterator.next().map(|value| {
+                    position += 1;
+                    state.buffer.push(value);
+                });
+            })
         }
-        None => Stream::Nil,
+
+        self.position.replace(position);
+    }
+
+    pub fn get(&self, target: usize) -> Option<&T> {
+        let position = *self.get_position();
+
+        if target > position {
+            None
+        } else {
+            self.buffer.get(target - position)
+        }
     }
 }
 
-fn vec_to_stream() {
-    let vec = vec![1, 2, 3];
-    let cell = RefCell::new(vec.into_iter());
+pub struct Stream<T> {
+    state: State<T>,
+    cursors: RefCell<Vec<Weak<RefCell<Cursor>>>>,
+}
 
-    iter_to_stream(&cell);
+impl<T> Stream<T> {
+    pub fn new(iterator: Box<dyn Iterator<Item = T>>) -> Self {
+        Stream {
+            state: State::new(iterator),
+            cursors: RefCell::new(vec![]),
+        }
+    }
+
+    pub fn add_cursor(self, cursor: Rc<RefCell<Cursor>>) {
+        self.cursors.borrow_mut().push(Rc::downgrade(&cursor));
+    }
+
+    pub fn next(&mut self, cursor: Rc<RefCell<Cursor>>) -> Option<&T> {
+        let mut cursor = cursor.borrow_mut();
+        let next_position = cursor.position + 1;
+
+        self.state.advance_to(next_position);
+
+        self.state.get(next_position).map(|next| {
+            cursor.position = next_position;
+            next
+        })
+    }
+}
+
+pub struct Cursor {
+    position: usize,
+}
+
+impl Cursor {
+    pub fn new<T>(stream: &Stream<T>) -> Rc<RefCell<Self>> {
+        let cursor = Rc::new(RefCell::new(Cursor { position: 0 }));
+
+        stream.cursors.borrow_mut().push(Rc::downgrade(&cursor));
+
+        cursor
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let vec = vec![1, 2, 3];
+
+        let mut stream = Stream::new(Box::new(vec.into_iter()));
+        let cursor = Cursor::new(&stream);
+
+        assert_eq!(stream.next(cursor), Some(&1));
+    }
 }
